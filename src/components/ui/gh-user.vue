fe8055e89cfeea9cc7832c07fda6e18cc3315f90
Jagger <634750802@qq.com>
2022-09-02 14:44:13 +0800

    fix bot link

--- src/components/ui/gh-user.vue
+++ src/components/ui/gh-user.vue
@@ -1,8 +1,25 @@
 <template>
-  <a :href="`https://github.com/${props.login}`" target="_blank" :class="{new: props.isNew}">{{ props.login }}</a>
+  <a :href="href" target="_blank" :class="{new: props.isNew}">{{ props.login }}</a>
 </template>
 <script setup lang="ts">
-const props = defineProps<{ login: string, isNew?: boolean }>();
+import { computed } from "vue";
+
+const props = defineProps<{
+  login: string
+  isNew?: boolean
+  url?: string
+}>();
+
+const BOT_REGEXP = /\[bot]$/
+
+const href = computed(() => {
+  const { login } = props
+  if (BOT_REGEXP.test(login)) {
+    return `https://github.com/apps/${login.replace(BOT_REGEXP, '')}`
+  } else {
+    return `https://github.com/${login}`
+  }
+})
 </script>
 <style scoped>
 </style>
