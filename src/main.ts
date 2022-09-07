aa1a5a9dd63935ca766c572887a504168dad964b
Jagger <634750802@qq.com>
2022-09-07 17:22:06 +0800

    add sentry

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,22 @@ import App from './app.vue';
 import store, { installResize } from "./store";
 import { SYMBOL_VISIBLE, SYMBOL_VISIBLE_INSTANT, visible, VISIBLE_THROTTLE } from "@/plugins/visible";
 import router from "@/router";
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
 
 const app = createApp(App);
+Sentry.init({
+  app,
+  dsn: "https://d326eccb0d414397a2cbfbc4f7271823@o226447.ingest.sentry.io/6726051",
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ["live.ossinsight.io"],
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 app
   .use(store)
   .use(router)
