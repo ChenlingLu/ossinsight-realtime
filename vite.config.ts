cb439e173c4ed1b499cae995934e5b7341595436
Jagger <634750802@qq.com>
2022-09-07 15:18:52 +0800

    update meta

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from 'path';
 import svgLoader from 'vite-svg-loader';
+import _htmlTemplate from "vite-plugin-html-template";
 
+const htmlTemplate = (_htmlTemplate as any).default as typeof _htmlTemplate
 export default defineConfig({
   plugins: [
     vue(),
@@ -22,6 +24,15 @@ export default defineConfig({
             },
           },
         ],
+      }
+    }),
+    htmlTemplate({
+      data: {
+        title: 'Real-time GitHub Contribution City 2022 | OSS Insight',
+        description: 'A 3D city created with GitHub contribution graph, you can see how developers build the open source world with corporations in real-time.',
+        keywords: 'GitHub contribution graph,3d GitHub contribution,GitHub city,Real-time GitHub Contribution,live GitHub contribution city,open-source corporation,data flow,pull requests in GitHub',
+        home: 'https://live.ossinsight.io/',
+        image: '/thumbnail.png',
       }
     })
   ],
