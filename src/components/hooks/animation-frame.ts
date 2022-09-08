65af4b0aa860732e3f7a4d431cb387d8117f3a52
Jagger <634750802@qq.com>
2022-09-08 18:14:08 +0800

    optimize

--- src/components/hooks/animation-frame.ts
+++ src/components/hooks/animation-frame.ts
@@ -1,39 +1,78 @@
+import { animationFrames, mergeMap, Observable, Subscriber, Subscription } from "rxjs";
+import { createDebugLogger } from "@/utils/debug";
+import { onBeforeUnmount } from "vue";
+
 const FPS = 60;
-const FRAME_INTERVAL = 1 / FPS;
+const FRAME_INTERVAL = 1000 / FPS;
+
+class UnifiedAnimationFrameSource extends Observable<readonly [number, number]> {
+  subscribers: Set<Subscriber<readonly [number, number]>> = new Set();
+  subscription?: Subscription;
+
+  constructor() {
+    const debug = createDebugLogger('[animation-frame-source]');
+    super(subscriber => {
+      debug('subscribe');
+      const shouldStart = this.subscribers.size === 0;
+      this.subscribers.add(subscriber);
+
+      if (shouldStart) {
+        debug('start');
+        let last = performance.now();
+        let delta = 0;
+        const frames = animationFrames().pipe(mergeMap(({ timestamp }) => {
+          const diff = timestamp - last;
+          last = timestamp;
+          delta += diff;
+          if (delta >= FRAME_INTERVAL) {
+            const res = [[timestamp, delta] as const];
+            delta %= FRAME_INTERVAL;
+            return res;
+          }
+          return [];
+        }));
+        this.subscription = frames.subscribe((tick) => {
+          this.subscribers.forEach(s => s.next(tick));
+        });
+      }
+
+      subscriber.add(() => {
+        debug('unsubscribe');
+        this.subscribers.delete(subscriber);
+        const shouldStop = this.subscribers.size === 0;
+        if (shouldStop) {
+          debug('stop');
+          this.subscription?.unsubscribe();
+          this.subscription = undefined;
+        }
+      });
+    });
+  }
+}
+
+const source = new UnifiedAnimationFrameSource();
 
 export function useAnimationFrame(cb: (ts: DOMHighResTimeStamp, diff: DOMHighResTimeStamp) => void) {
-  let started = false;
-  let h: undefined | number;
-  let last: DOMHighResTimeStamp;
-  let delta = 0;
-
-  const _cb = (ts: DOMHighResTimeStamp) => {
-    let diff = ts - last;
-    delta += diff;
-    last = ts;
-    if (delta >= FRAME_INTERVAL) {
-      cb(ts, diff);
-    }
-    if (started) {
-      h = requestAnimationFrame(_cb);
-    }
-  };
+  let subscription: Subscription | undefined;
 
   const start = () => {
-    if (started) {
+    if (subscription) {
       return;
     }
-    started = true;
-    last = performance.now();
-    h = requestAnimationFrame(_cb);
+
+    subscription = source.subscribe(([ts, diff]) => {
+      cb(ts, diff);
+    });
   };
 
   const stop = () => {
-    started = false;
-    if (h) {
-      cancelAnimationFrame(h);
+    if (subscription) {
+      subscription.unsubscribe();
+      subscription = undefined;
     }
   };
+
+  onBeforeUnmount(stop);
 
   return { start, stop };
 }
