572712a0e800659511df555d9f2d092f46cf4583
Jagger <634750802@qq.com>
2022-09-14 14:27:39 +0800

    add seo

--- vite.config.ts
+++ vite.config.ts
@@ -50,10 +50,10 @@ export default defineConfig({
           template: 'template-2d.html',
           inject: {
             data: {
-              title: 'Real-time GitHub Contribution City 2022 | OSS Insight',
-              description: 'A 3D city created with GitHub contribution graph, you can see how developers build the open source world with corporations in real-time.',
-              keywords: 'GitHub contribution graph,3d GitHub contribution,GitHub city,Real-time GitHub Contribution,live GitHub contribution city,open-source corporation,data flow,pull requests in GitHub',
-              home: 'https://live.ossinsight.io/',
+              title: 'Real-time GitHub Contribution Insight | OSS Insight ',
+              description: 'Here you can find some insights into GitHub real-time contribution, including pull request events grouped by programming languages, top 5 active repositories and developers last 24 hours.',
+              keywords: 'Open Source,GitHub contribution, GitHub Trending, GitHub, pull request, real time, Open Source Programming Language',
+              home: 'https://live.ossinsight.io/detail',
               domain: 'live.ossinsight.io',
               image: 'thumbnail.png',
               gtag: 'G-KW4FDPBLLJ',
